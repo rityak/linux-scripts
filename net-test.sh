3d2e8632653fcb22c88875e556fbb857910edf21
Ritya <33477987+rityak@users.noreply.github.com>
2024-08-05 12:31:42 +0300

    Update net-test.sh

--- net-test.sh
+++ net-test.sh
@@ -14,21 +14,14 @@ install_packages() {
     done
 }
 
-# Удаление старого и установка нового Speedtest CLI
-echo "Удаление старого Speedtest CLI и установка нового..."
-if [ -f /etc/apt/sources.list.d/speedtest.list ]; then
-    sudo rm /etc/apt/sources.list.d/speedtest.list
-fi
-sudo apt-get update
-sudo apt-get remove -y speedtest speedtest-cli
-
-# Установка нового Speedtest CLI
-sudo apt-get install -y curl
-curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
-sudo apt-get install -y speedtest
-
 # Установка недостающих пакетов
 install_packages
+
+# Установка Speedtest CLI локально из архива
+echo "Установка Speedtest CLI..."
+wget -q "https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-x86_64.tgz" -O speedtest.tgz
+tar xzfv speedtest.tgz
+chmod +x speedtest
 
 # Функция для красивого вывода разделителя
 print_separator() {
@@ -37,41 +30,57 @@ print_separator() {
 
 # Проверка MTU
 echo "Проверка MTU..."
-MTU=$(ip link show | grep mtu | awk '{print $5}')
+MTU=$(ip link show | grep mtu | awk '{print $5}' | tr '\n' ' ')
+MTU=${MTU::-1}
 echo "MTU: $MTU"
 
 # Проверка IP адресов
 echo "Проверка IP адресов..."
-IP_ADDRESSES=$(hostname -I)
+IP_ADDRESSES=$(hostname -I | tr ' ' '\n' | awk '{print "    " $0}' | tr '\n' ' ')
+IP_ADDRESSES=${IP_ADDRESSES::-1}
 echo "IP адреса: $IP_ADDRESSES"
 
 # Проверка скорости интернета
 print_separator
 echo "Проверка скорости интернета..."
-SPEEDTEST_OUTPUT=$(speedtest --format=json)
+SPEEDTEST_OUTPUT=$(./speedtest --format=json)
 
 DOWNLOAD_SPEED=$(echo $SPEEDTEST_OUTPUT | jq '.download.bandwidth' | awk '{print $1/125000 " Mbps"}')
 UPLOAD_SPEED=$(echo $SPEEDTEST_OUTPUT | jq '.upload.bandwidth' | awk '{print $1/125000 " Mbps"}')
 PING=$(echo $SPEEDTEST_OUTPUT | jq '.ping.latency' | awk '{print $1 " ms"}')
 
-# Проверка доступности DNS серверов
 print_separator
-echo "Проверка доступности DNS серверов..."
+echo "Резюме диагностики сети"
+print_separator
+echo "1. MTU: $MTU"
+echo "2. IP адреса: $IP_ADDRESSES"
+echo "3. Скорость интернета:"
+echo "   Download: $DOWNLOAD_SPEED"
+echo "   Upload: $UPLOAD_SPEED"
+echo "   Ping: $PING"
+print_separator
+
+# Проверка доступности DNS серверов
+echo "4. Доступность DNS серверов:"
 DNS_SERVERS=("8.8.8.8" "8.8.4.4" "1.1.1.1")
 DNS_RESULTS=()
 
 for DNS_SERVER in "${DNS_SERVERS[@]}"; do
     dig @$DNS_SERVER google.com +short > /dev/null 2>&1
     if [ $? -eq 0 ]; then
-        DNS_RESULTS+=("DNS сервер $DNS_SERVER доступен.")
+        DNS_RESULTS+=("   DNS сервер $DNS_SERVER доступен.")
     else
-        DNS_RESULTS+=("DNS сервер $DNS_SERVER недоступен.")
+        DNS_RESULTS+=("   DNS сервер $DNS_SERVER недоступен.")
     fi
 done
 
-# Проверка качества соединения до нескольких локаций
+for RESULT in "${DNS_RESULTS[@]}"; do
+    echo "$RESULT"
+done
 print_separator
-echo "Проверка качества соединения до нескольких локаций..."
+
+# Проверка качества соединения до нескольких локаций
+echo "5. Качество соединения до локаций:"
 LOCATIONS=("google.com" "cloudflare.com" "yandex.ru")
 MTR_RESULTS=()
 
@@ -80,15 +89,20 @@ for LOCATION in "${LOCATIONS[@]}"; do
     MTR_RESULTS+=("$MTR_RESULT")
 done
 
+for RESULT in "${MTR_RESULTS[@]}"; do
+    echo "$RESULT"
+    print_separator
+done
+
 # Проверка доступности популярных сервисов
 check_service_availability() {
     SERVICE=$1
     HOST=$2
     nc -z -v -w5 $HOST 80 &> /dev/null
     if [ $? -eq 0 ]; then
-        echo "$SERVICE доступен."
+        echo "   $SERVICE доступен."
     else
-        echo "$SERVICE недоступен."
+        echo "   $SERVICE недоступен."
     fi
 }
 
@@ -109,60 +123,33 @@ for SERVICE in "${SERVICES[@]}"; do
     SERVICE_RESULTS+=("$RESULT")
 done
 
-# Вывод резюме
-print_separator
-echo "Резюме диагностики сети"
-print_separator
-echo "1. MTU: $MTU"
-echo "2. IP адреса: $IP_ADDRESSES"
-echo "3. Скорость интернета:"
-echo "   Download: $DOWNLOAD_SPEED"
-echo "   Upload: $UPLOAD_SPEED"
-echo "   Ping: $PING"
-print_separator
-echo "4. Доступность DNS серверов:"
-for RESULT in "${DNS_RESULTS[@]}"; do
-    echo "   $RESULT"
-done
-print_separator
-echo "5. Качество соединения до локаций:"
-for RESULT in "${MTR_RESULTS[@]}"; do
-    echo "$RESULT"
-    print_separator
-done
 echo "6. Доступность популярных сервисов:"
 for RESULT in "${SERVICE_RESULTS[@]}"; do
-    echo "   $RESULT"
+    echo "$RESULT"
 done
 print_separator
 
-# Дополнительные тесты и полезная информация
-
 # Проверка настроек DNS
-echo "Проверка настроек DNS..."
-DNS_CONFIG=$(cat /etc/resolv.conf)
 echo "Настройки DNS:"
+DNS_CONFIG=$(cat /etc/resolv.conf | grep -v '^#' | grep -v '^$')
 echo "$DNS_CONFIG"
 print_separator
 
 # Проверка сетевой статистики
-echo "Проверка сетевой статистики..."
-NETSTAT_OUTPUT=$(netstat -i)
 echo "Сетевая статистика:"
+NETSTAT_OUTPUT=$(netstat -i | awk 'NR>2 {print}')
 echo "$NETSTAT_OUTPUT"
 print_separator
 
 # Проверка таблицы маршрутизации
-echo "Проверка таблицы маршрутизации..."
-ROUTE_OUTPUT=$(ip route)
 echo "Таблица маршрутизации:"
+ROUTE_OUTPUT=$(ip route | awk '{print "    " $0}')
 echo "$ROUTE_OUTPUT"
 print_separator
 
 # Проверка сетевых подключений
-echo "Проверка сетевых подключений..."
-SS_OUTPUT=$(ss -tuln)
 echo "Сетевые подключения:"
+SS_OUTPUT=$(ss -tuln | awk 'NR>1 {print "    " $0}')
 echo "$SS_OUTPUT"
 print_separator
 
